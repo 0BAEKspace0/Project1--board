6e9977b1bd12caf0a16344010970902cdeaf06d6
kimjh <hecticika@gmail.com>
2022-11-25 16:27:01 +0900

    feat: List 페이징 기능 구현

--- public/js/list.js
+++ public/js/list.js
@@ -1,18 +1,20 @@
-const item = localStorage.getItem("boards")
-
-if(item === null){
-    const intialState = []
-    const state = JSON.stringify(intialState)
-    localStorage.setItem("boards", state)
-    item = state    
+let item = localStorage.getItem("boards");
+if (item === null) {
+  const initialState = [];
+  const state = JSON.stringify(initialState);
+  localStorage.setItem("boards", state);
+  item = state;
 }
 
-function template (item, index){
-    return `
+item = localStorage.getItem("boards");
+
+const tbody = document.querySelector("tbody");
+
+function template(item, index) {
+  return `
     <tr>
-        <td>${index +1}</td>
-        <td><a href="/board/view.html?index=${index}">${item.subject}</a></td>
-        <td>${item.content}</td>
+        <td>${index + 1}</td>
+        <td><a href='../board/view.html?index=${index}'>${item.subject}</a></td>
         <td>${item.writer}</td>
         <td>${item.date}</td>
         <td>${item.hit}</td>
@@ -20,8 +22,64 @@ function template (item, index){
     `
 }
 
-const tbody = document.querySelector("tbody")
-const boards = JSON.parse(item)
-for(let i = boards.length -1 ; i >=0 ; i--){
-    tbody.innerHTML=tbody.innerHTML+template(boards[i], i)   
+
+const boards = JSON.parse(item);
+const paging = document.querySelector("#paging")
+const prev = document.querySelector(".prev")
+const next = document.querySelector(".next")
+
+const Idx = location.search.split('=')
+const pageIndex = Number(Idx[1])
+
+let lastPage = 0
+if (boards.length%10 == 0) {
+  lastPage = parseInt(boards.length/10)
+} else if (boards.length%10 != 0) {
+  lastPage = parseInt(boards.length/10+1)
 }
+
+function pageTemplate () {
+  let pageArr = []
+  for (i=0; i<lastPage; i++) {
+    pageArr.push(`<a href="../board/list.html?index=${i}" class="pageMove">${i+1}</a>`)
+  } return pageArr
+}
+paging.innerHTML += pageTemplate()
+
+
+
+function prevHandler(e) {
+  if (pageIndex > 0) {
+    location.href = "../board/list.html?index=" + (pageIndex - 1)
+  } else return alert("최신 페이지입니다.")
+}
+
+function nextHandler(e) {
+  if (pageIndex < lastPage - 1) {
+    location.href = "../board/list.html?index=" + (pageIndex + 1)
+  } else return alert("마지막 페이지입니다.")
+}
+
+prev.addEventListener ("click", prevHandler)
+next.addEventListener ("click", nextHandler)
+
+
+if (pageIndex == 0) {
+  for (let i = boards.length - 1; i > boards.length-11; i--) {
+      tbody.innerHTML += template(boards[i], i);
+  }
+}
+  
+if (pageIndex == 1) {
+  for (let i = boards.length - 11; i > boards.length-21; i--) {
+      tbody.innerHTML += template(boards[i], i);
+  }
+}
+
+if (pageIndex == 2) {
+
+  for (let i = boards.length - 21; i >= 0; i--) {
+      tbody.innerHTML += template(boards[i], i);
+  }
+}
+
