08d04896e7b8a5335326c7db6c74fb99e4f2a715
baek <baekbr13@gmail.com>
2022-11-25 14:56:55 +0900

    feat: list.js 기능 구현

--- public/js/list.js
+++ public/js/list.js
@@ -6,3 +6,22 @@ if(item === null){
     localStorage.setItem("boards", state)
     item = state    
 }
+
+function template (item, index){
+    return `
+    <tr>
+        <td>${index +1}</td>
+        <td><a href="/board/view.html?index=${index}">${item.subject}</a></td>
+        <td>${item.content}</td>
+        <td>${item.writer}</td>
+        <td>${item.date}</td>
+        <td>${item.hit}</td>
+    </tr>
+    `
+}
+
+const tbody = document.querySelector("tbody")
+const boards = JSON.parse(item)
+for(let i = boards.length -1 ; i >=0 ; i--){
+    tbody.innerHTML=tbody.innerHTML+template(boards[i], i)   
+}
