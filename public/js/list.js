a7b04b315799b72e7cd19932c1de586c35e81d8e
baek <baekbr13@gmail.com>
2022-11-25 14:35:32 +0900

    feat: list, write 기본 기능 구현

---
+++ public/js/list.js
@@ -0,0 +1,8 @@
+const item = localStorage.getItem("boards")
+
+if(item === null){
+    const intialState = []
+    const state = JSON.stringify(intialState)
+    localStorage.setItem("boards", state)
+    item = state    
+}
